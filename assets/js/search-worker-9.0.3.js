7d41da195d460871a4f8bbeb4cc608cbec7ed253
cgirerd <cgirerd@X220-THINK.lan>
2020-07-11 16:40:01 -0700

    Re-enable cover for welcome page

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-07-11T16:17:15-07:00';
+const INDEX_KEY = 'index--2020-07-11T16:39:26-07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
