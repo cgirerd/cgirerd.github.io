4a24dcf6a73b47762310f7b1b5007ef6f9a02378
cgirerd <cgirerd@X220-THINK.lan>
2020-07-11 02:40:14 -0700

    Fix photo in resume

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.3--v7--sw/";
 const ASSETS_CACHE = "assets--v7--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2020-07-11T02:31:22-07:00--sw/";
+const CONTENT_CACHE = "content--2020-07-11T02:39:58-07:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -70,6 +70,7 @@ const STATIC_FILES = [
   /**/"/assets/papers/girerd20ral.pdf",
   /**/"/assets/papers/girerd20tmrb.mp4",
   /**/"/assets/papers/stent15isarc.pdf",
+  /**/"/netlify.toml",
   /**/"/assets/bower.json",
   /**/"/assets/bower_components/MathJax/.bower.json",
   /**/"/assets/bower_components/MathJax/LICENSE",
