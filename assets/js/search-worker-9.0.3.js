069fc2e1a8df8f2f6311e501dde8c03d56ab30bc
cgirerd <cgirerd@X220-THINK.lan>
2020-07-11 02:31:36 -0700

    Fix photo in resume

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-07-11T02:24:14-07:00';
+const INDEX_KEY = 'index--2020-07-11T02:31:22-07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
