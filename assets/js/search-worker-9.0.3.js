542ed3f958add1f57649220f382b33d03947ec5a
cgirerd <cgirerd@X220-THINK.lan>
2020-07-14 00:16:49 -0700

    T-MRB video added

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-07-13T19:40:31-07:00';
+const INDEX_KEY = 'index--2020-07-14T00:16:34-07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
