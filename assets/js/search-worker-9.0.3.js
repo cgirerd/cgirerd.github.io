f577b76973493bd4f5350c93a9804a08cf00efe5
cgirerd <cgirerd@X220-THINK.lan>
2020-08-16 01:57:19 -0700

    academicons logos updated

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-07-14T13:35:51-07:00';
+const INDEX_KEY = 'index--2020-08-16T01:56:39-07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
