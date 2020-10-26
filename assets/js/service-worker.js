365dc1a7c774ff140db994f487794eec9b382040
cgirerd <cgirerd@X220-THINK.lan>
2020-10-26 14:59:46 -0700

    sidebar color changed

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.3--v7--sw/";
 const ASSETS_CACHE = "assets--v7--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2020-10-26T14:53:27-07:00--sw/";
+const CONTENT_CACHE = "content--2020-10-26T14:59:31-07:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -69,6 +69,7 @@ const STATIC_FILES = [
   /**/"/assets/papers/girerd20ral.mp4",
   /**/"/assets/papers/girerd20ral.pdf",
   /**/"/assets/papers/girerd20tmrb.mp4",
+  /**/"/assets/papers/girerd20tmrb.pdf",
   /**/"/assets/papers/stent15isarc.pdf",
   /**/"/netlify.toml",
   /**/"/assets/bower.json",
